44f8f9ebcbac557e625e0c9df9a726ffbe4d8b7a
منى الحضرمي <mnyalhdrmy@mnys-MacBook-Pro.local>
2022-07-28 12:08:00 +0400

    modified unset solve error

--- srcs/builtins/unset.c
+++ srcs/builtins/unset.c
@@ -1,37 +1,23 @@
 #include "../../includes/minishell.h"
 
-int	ft_is_in_env2(char *key, t_data *data)
+int	ft_in_env(t_data *data, char *key)
 {
 	int	i;
 	int	size;
 
 	i = -1;
 	size = ft_strlen(key);
-    // printf("size : %d\n",size);
 	while (data->env[++i])
 	{
-		if (ft_strncmp(key, data->env[i], size) == 0)
+		if (ft_strncmp(key, data->env[i], size - 1) && size > 1)
+			return (1);
+        else if (size <= 1 && data->env[i][0] == key[0])
 			return (1);
 	}
 	return (0);
 }
 
-// char	*get_key(t_data *data)
-// {
-// 	int		i;
-// 	char	*key;
-
-// 	i = 0;
-// 	while (data->env[i] && (ft_isalnum(data->env[i]) || data->env[i] == '='))
-// 	{
-// 		if (data->env[i] == '=')
-// 			break ;
-// 		i++;
-// 	}
-// 	key = ft_strndup(data->env, i);
-// 	return (key);
-// }
-char    **unset_in_env(char *key, t_data *data)
+char    **unset_in_env(char *key,t_data *data)
 {
     int i;
     int j;
@@ -43,19 +29,30 @@ char    **unset_in_env(char *key, t_data *data)
     j = 0;
     size = 0;
     len_key = ft_strlen(key);
+    printf("its work\n");
     while (data->env[++i])
         size++;
     temp = malloc(sizeof(char *) * (size + 1));
     i = -1;
     while (data->env[++i])
     {
-        if (!(ft_strncmp(key, data->env[i], len_key - 1) == 0))
-			temp[j++] = ft_strdup(data->env[i]);
+        if (len_key > 1)
+        {
+            if (!(ft_strncmp(key, data->env[i], len_key - 1) == 0))
+			    temp[j++] = ft_strdup(data->env[i]);
+        }
+        if (len_key == 1)
+        {
+            if (!(len_key <= 1 && key[0] == data->env[i][0]))
+                temp[j++] = ft_strdup(data->env[i]);
+        }
+        
     }
     free_env(data->env);
 	temp[j] = NULL;
 	return (temp);
 }
+
 char    **ft_unset(t_data *data, int num_cmd)
 {
     char *key;
@@ -72,7 +69,7 @@ char    **ft_unset(t_data *data, int num_cmd)
     while (data->pars[num_cmd].cmd[++i])
     {
         ft_parse_env(data->pars[num_cmd].cmd[i], &key, &value);
-        if (ft_is_in_env(data, key))
+        if (ft_in_env(data, key))
             data->env = unset_in_env(key, data);
         return(data->env);
     }
