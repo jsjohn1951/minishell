0c4757639f2d59373420fe7cc19f36f8affe2b38
Willem Smith <wismith@lab1r1s4.42abudhabi.ae>
2022-08-01 18:59:01 +0400

    should not work with heredoc

--- srcs/expansions/exp_tools.c
+++ srcs/expansions/exp_tools.c
@@ -6,7 +6,7 @@
 /*   By: wismith <wismith@42ABUDHABI.AE>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/01 18:41:34 by wismith           #+#    #+#             */
-/*   Updated: 2022/08/01 18:45:36 by wismith          ###   ########.fr       */
+/*   Updated: 2022/08/01 18:57:18 by wismith          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ void	exit_status_exp_(t_data *data, t_expand *exp, int j)
 	char	*tmp;
 	char	*res;
 
+	if (!ft_strncmp(data->pars[exp->par_i].pipe_redir, "<<", 2))
+		return ;
 	tmp = ft_substr(data->pars[exp->par_i].cmd[j], 0, exp->in);
 	res = ft_strjoin(tmp, ft_itoa(data->err));
 	res = ft_strjoin_mod(res, data->pars[exp->par_i].cmd[j] + exp->in + 2,
