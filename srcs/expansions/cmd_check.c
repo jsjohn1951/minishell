08c3cf955e979ee058a60fe1cfbccd49830aa0a2
Willem Smith <wismith@lab1r1s4.42abudhabi.ae>
2022-08-01 16:10:44 +0400

    making progress with expansions

--- srcs/expansions/cmd_check.c
+++ srcs/expansions/cmd_check.c
@@ -6,7 +6,7 @@
 /*   By: wismith <wismith@42ABUDHABI.AE>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/01 14:16:47 by wismith           #+#    #+#             */
-/*   Updated: 2022/08/01 14:44:21 by wismith          ###   ########.fr       */
+/*   Updated: 2022/08/01 16:04:01 by wismith          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,25 +32,59 @@ int	is_dollar_(char *s)
 	return (0);
 }
 
-void	matrix_check_(t_data *data, int i)
+char	*key_(char *s, int in)
 {
-	int	j;
+	int		i;
+	char	*key;
 
-	j = -1;
-	while (data->pars[i].cmd[++j])
-		if (is_dollar_(data->pars[i].cmd[j]))
-			printf("found dollar! :)\n");
+	i = in + 1;
+	while (s[i])
+	{
+		if (s[i] == ' ' || is_quote_(s[i]) || s[i] == '$')
+			break ;
+		i++;
+	}
+	if (i > 0 && i > in + 1)
+		key = ft_substr(s, in + 1, i - in - 1);
+	else
+		key = NULL;
+	return (key);
+}
+
+void	expand(t_data *data, int i, int j)
+{
+	int		in;
+	char	*key;
+
+	in = 0;
+	while (data->pars[i].cmd[j][in])
+	{
+		if (data->pars[i].cmd[j][in] == '$')
+		{
+			key = key_(data->pars[i].cmd[j], in);
+			if (key)
+				printf("key: %s\n", key);
+			else
+				printf("key: NULL\n");
+			ft_free(key);
+		}
+		in++;
+	}
 }
 
 void	expandable_check_(t_data *data)
 {
 	int	i;
+	int	j;
 
 	i = 0;
+	j = -1;
 	while (i < data->num_cmds)
 	{
 		if (data->pars[i].cmd)
-			matrix_check_(data, i);
+			while (data->pars[i].cmd[++j])
+				if (is_dollar_(data->pars[i].cmd[j]))
+					expand(data, i, j);
 		i++;
 	}
 }
