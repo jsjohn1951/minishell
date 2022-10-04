4088a2120e2e1e28a992b3d21bbd3ac04feed3be
jsjohn1951 <jsjohn1951@users.noreply.github.com>
2022-10-04 11:47:13 +0400

    fixed segfault

--- srcs/expand/tools2.c
+++ srcs/expand/tools2.c
@@ -6,7 +6,7 @@
 /*   By: wismith <wismith@42ABUDHABI.AE>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/01 22:02:10 by wismith           #+#    #+#             */
-/*   Updated: 2022/10/01 22:12:57 by wismith          ###   ########.fr       */
+/*   Updated: 2022/10/04 11:43:48 by wismith          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,8 +20,12 @@ char	*ft_insert_quote(char *s)
 	i = -1;
 	res = ft_strdup(s);
 	while (s && res && res[++i])
+	{
 		if (res[i] == 11)
 			res[i] = 34;
+		else if (res[i] == 6)
+			res[i] = 39;
+	}
 	s = ft_free (s);
 	return (res);
 }
@@ -34,8 +38,12 @@ char	*ft_replace_quote(char *s)
 	res = ft_strdup(s);
 	i = -1;
 	while (s && res && res[++i])
+	{
 		if (res[i] == 34)
 			res[i] = 11;
+		else if (res[i] == 39)
+			res[i] = 6;
+	}
 	s = ft_free (s);
 	return (res);
 }
